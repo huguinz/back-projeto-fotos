013f0f6235884f761e575f1015311277b0d864d8
HUGO LOPES DE SOUZA <24122289@dsn127m1247443.cfp127aluno.local>
2025-08-12 09:37:15 -0300

    projeto finalizado

--- module/app.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-export const getPhotos = async () => {
-    const url = 'http://localhost:3000/fotos'
-    const response = await fetch(url)
-    const data = await response.json()
-
-    return data
-}
